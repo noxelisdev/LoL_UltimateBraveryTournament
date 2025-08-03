cecd3616895f759d53d7fe0ed33218f21c5aa692
InFinity_54 <corentinkreicher@gmail.com>
2025-08-03 07:03:47 +0200

    Création d'un nouveau projet vierge sous Electron-Vite-Vue

--- src/js/common/video.js
+++
@@ -1,9 +0,0 @@
-function playMovie(htmlElement) {
-  const videoPlaying = htmlElement.play();
-
-  if (videoPlaying !== undefined) {
-    videoPlaying.then(_ => {}).catch(error => {});
-  }
-}
-
-export { playMovie };
