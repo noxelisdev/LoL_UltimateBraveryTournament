d340d938a5dbbd408c1580541e4981f433ef6629
Noxelis <corentinkreicher@gmail.com>
2026-03-12 22:37:46 +0100

    Correction de vulnérabilités

--- src/renderer/src/components/Homepage.vue
+++ src/renderer/src/components/Homepage.vue
@@ -37,6 +37,7 @@ function tabClick() {
             corrections ont été apportées au programme. <i class="patchnotes_quoteend"></i>
           </p>
           <ul>
+            <li>12/03/2026 : Correction de vulnérabilités dans plusieurs dépendances</li>
             <li>03/03/2026 : Correction d'une vulnérabilité dans une dépendance</li>
             <li>27/02/2026 : Correction d'une vulnérabilité dans une dépendance</li>
             <li>
